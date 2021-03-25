7baf3e4c1bee55ea7c1af0889d0e58d41cb06c09
Stefan Mertl <info@mertl-research.at>
2021-03-25 17:32:38 +0100

    Removed the plugin. It corrupted the mathjax syntax tag. Create advanced tables using yaml data. This would also be better for re-using the tables.

--- _plugins/add_table_caption.rb
+++
@@ -1,6 +0,0 @@
-class Jekyll::MarkdownCaption < Jekyll::Converters::Markdown
-  def convert(content)
-      super
-        .gsub(/<\/table>\n+<p>Table: (.*?)<\/p>/, '<caption>\1</caption></table>')
-  end
-end
